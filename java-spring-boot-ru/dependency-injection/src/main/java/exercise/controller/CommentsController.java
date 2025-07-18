aa7fd954b34d3b941c0c1cbac31efa8bf437a530
@hexlet/cli <support@hexlet.io>
2025-07-18 19:54:24 +0300

    submit java-spring-boot-ru/dependency-injection

--- java-spring-boot-ru/dependency-injection/src/main/java/exercise/controller/CommentsController.java
+++ java-spring-boot-ru/dependency-injection/src/main/java/exercise/controller/CommentsController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
--- java-spring-boot-ru/dependency-injection/src/main/java/exercise/controller/PostsController.java
+++ java-spring-boot-ru/dependency-injection/src/main/java/exercise/controller/PostsController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.http.HttpStatus;
 import java.util.List;
+import org.springframework.http.ResponseEntity;
 
 import exercise.model.Post;
 import exercise.repository.PostRepository;
