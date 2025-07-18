e48eb1b7c90b443de4e19db577cbf00d0974c4ed
@hexlet/cli <support@hexlet.io>
2025-07-18 20:19:47 +0300

    submit java-spring-boot-ru/beans

---
+++ java-spring-boot-ru/beans/src/main/java/exercise/Application.java
@@ -0,0 +1,34 @@
+package exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDateTime;
+
+import exercise.daytime.Daytime;
+import exercise.daytime.Day;
+import exercise.daytime.Night;
+
+// BEGIN
+import org.springframework.context.annotation.Bean;
+// END
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    // BEGIN
+    @Bean
+    public Daytime daytime() {
+        LocalTime now = LocalTime.now();
+        if (now.isAfter(LocalTime.of(5, 59)) && now.isBefore(LocalTime.of(22, 0))) {
+            return new Day();
+        } else {
+            return new Night();
+        }
+    }
+    // END
+}
