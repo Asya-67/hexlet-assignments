bb96fa998159f1fa85d0b7eaad540064264a1c76
@hexlet/cli <support@hexlet.io>
2025-07-18 16:18:49 +0300

    submit java-spring-boot-ru/jpa-comparing-entities

--- java-spring-boot-ru/jpa-comparing-entities/src/main/java/exercise/exception/ResourceAlreadyExistsException.java
+++ java-spring-boot-ru/jpa-comparing-entities/src/main/java/exercise/exception/ResourceAlreadyExistsException.java
@@ -1,8 +1,8 @@
 package exercise.exception;
 
 // BEGIN
-public class ResourceAlreadyExistException extends RuntimeException {
-    public ResourceAlreadyExistException(String message) {
+public class ResourceAlreadyExistsException extends RuntimeException {
+    public ResourceAlreadyExistsException(String message) {
         super(message);
     }
 }
--- java-spring-boot-ru/jpa-comparing-entities/src/main/java/exercise/model/Product.java
+++ java-spring-boot-ru/jpa-comparing-entities/src/main/java/exercise/model/Product.java
@@ -1,20 +1,14 @@
 package exercise.model;
-
-import jakarta.persistence.Id;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Table;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.AllArgsConstructor;
 // BEGIN
 @AllArgsConstructor
 @Getter
 @EqualsAndHashCode(of = {"title", "price"})
-class Product {
+public class Product {
     private String id;
     private String title;
     private int price;
