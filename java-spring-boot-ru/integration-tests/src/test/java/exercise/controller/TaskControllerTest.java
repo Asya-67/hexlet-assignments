bb8ae26d3dc0146e35c4d199797ccddd475d51a2
@hexlet/cli <support@hexlet.io>
2025-07-21 15:38:59 +0300

    submit java-spring-boot-ru/integration-tests

---
+++ java-spring-boot-ru/integration-tests/src/test/java/exercise/controller/TaskControllerTest.java
@@ -0,0 +1,144 @@
+package exercise.controller;
+
+import org.junit.jupiter.api.Test;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.instancio.Instancio;
+import org.instancio.Select;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+
+import java.util.HashMap;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.datafaker.Faker;
+import exercise.repository.TaskRepository;
+import exercise.model.Task;
+
+// BEGIN
+import org.springframework.transaction.annotation.Transactional;
+import java.util.Map;
+// END
+class TaskControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private Faker faker;
+
+    @Autowired
+    private ObjectMapper om;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+
+    @Test
+    public void testWelcomePage() throws Exception {
+        var result = mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        var body = result.getResponse().getContentAsString();
+        assertThat(body).contains("Welcome to Spring!");
+    }
+
+    @Test
+    public void testIndex() throws Exception {
+        var result = mockMvc.perform(get("/tasks"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        var body = result.getResponse().getContentAsString();
+        assertThatJson(body).isArray();
+    }
+
+
+    // BEGIN
+    @Test
+    public void testCreate() throws Exception {
+        Map<String, String> data = new HashMap<>();
+        data.put("title", faker.lorem().sentence(3));
+        data.put("description", faker.lorem().sentence(5));
+        var request = post("/tasks")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(om.writeValueAsString(data));
+
+        var result = mockMvc.perform(request)
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        var body = result.getResponse().getContentAsString();
+        assertThatJson(body).and(
+                a -> a.node("title").isEqualTo(data.get("title")),
+                a -> a.node("description").isEqualTo(data.get("description"))
+        );
+    }
+
+    private Task createSampleTask() {
+        Task task = new Task();
+        task.setTitle(faker.lorem().sentence(3));
+        task.setDescription(faker.lorem().sentence(6));
+        return taskRepository.save(task);
+    }
+
+    @Test
+    public void testShow() throws Exception {
+        Task task = createSampleTask();
+
+        var result = mockMvc.perform(get("/tasks/" + task.getId()))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        var body = result.getResponse().getContentAsString();
+        assertThatJson(body).and(
+                a -> a.node("id").isEqualTo(task.getId()),
+                a -> a.node("title").isEqualTo(task.getTitle()),
+                a -> a.node("description").isEqualTo(task.getDescription())
+        );
+    }
+
+    @Test
+    public void testUpdateTask() throws Exception {
+        Task task = createSampleTask();
+
+        Map<String, String> updateData = new HashMap<>();
+        updateData.put("title", faker.lorem().sentence(2));
+        updateData.put("description", faker.lorem().sentence(4));
+
+        var request = put("/tasks/" + task.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(om.writeValueAsString(updateData));
+
+        var result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andReturn();
+
+        var body = result.getResponse().getContentAsString();
+        assertThatJson(body).and(
+                a -> a.node("title").isEqualTo(updateData.get("title")),
+                a -> a.node("description").isEqualTo(updateData.get("description"))
+        );
+    }
+
+    @Test
+    public void testDeleteTask() throws Exception {
+        Task task = createSampleTask();
+
+        mockMvc.perform(delete("/tasks/" + task.getId()))
+                .andExpect(status().isNoContent());
+
+        var exists = taskRepository.findById(task.getId()).isPresent();
+        assertThat(exists).isFalse();
+    }
+    // END
+}
