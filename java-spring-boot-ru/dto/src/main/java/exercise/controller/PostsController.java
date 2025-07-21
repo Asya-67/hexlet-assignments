b68f970277718652be74fd3bf3b52b18a0aa2095
@hexlet/cli <support@hexlet.io>
2025-07-21 16:24:22 +0300

    submit java-spring-boot-ru/dto

---
+++ java-spring-boot-ru/dto/src/main/java/exercise/controller/PostsController.java
@@ -0,0 +1,36 @@
+package exercise.controller;
+
+import exercise.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
+
+import exercise.model.Post;
+import exercise.repository.PostRepository;
+import exercise.exception.ResourceNotFoundException;
+import exercise.dto.PostDTO;
+import exercise.dto.CommentDTO;
+
+// BEGIN
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @GetMapping("")
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Post getPostById(@PathVariable long id) {
+        return postRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Post with id " + id + " not found"));
+    }
+}
+// END
