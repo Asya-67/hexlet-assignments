b0ad1cfa4ce0051869637caac9220e8d78d879b2
@hexlet/cli <support@hexlet.io>
2025-07-22 17:15:03 +0300

    submit java-spring-boot-ru/dto-to-entity-for-update

--- java-spring-boot-ru/dto-to-entity-for-update/build.gradle.kts
+++ java-spring-boot-ru/dto-to-entity-for-update/build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("net.datafaker:datafaker:2.0.1")
     implementation("org.instancio:instancio-junit:3.3.0")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
