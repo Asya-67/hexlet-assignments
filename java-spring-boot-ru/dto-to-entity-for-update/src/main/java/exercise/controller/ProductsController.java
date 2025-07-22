3c6cb1306cba86bfe842e6f4dbc5ab00b9ef707e
@hexlet/cli <support@hexlet.io>
2025-07-22 17:09:43 +0300

    submit java-spring-boot-ru/dto-to-entity-for-update

--- java-spring-boot-ru/dto-to-entity-for-update/src/main/java/exercise/controller/ProductsController.java
+++ java-spring-boot-ru/dto-to-entity-for-update/src/main/java/exercise/controller/ProductsController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import jakarta.validation.Valid;
 
 import java.util.List;
 
