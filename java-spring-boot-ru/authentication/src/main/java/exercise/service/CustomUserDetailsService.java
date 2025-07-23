220f9410e6c1676649af3c95e5296e59e85e590a
@hexlet/cli <support@hexlet.io>
2025-07-23 20:57:29 +0300

    submit java-spring-boot-ru/authentication

--- java-spring-boot-ru/authentication/src/main/java/exercise/service/CustomUserDetailsService.java
+++ java-spring-boot-ru/authentication/src/main/java/exercise/service/CustomUserDetailsService.java
@@ -8,6 +8,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import exercise.model.User;
 
 // BEGIN
 @Service
