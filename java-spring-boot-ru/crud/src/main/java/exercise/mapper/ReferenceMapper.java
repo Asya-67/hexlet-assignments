210366248b9e9eac478d5e66f84b15ee40070771
@hexlet/cli <support@hexlet.io>
2025-07-23 16:52:03 +0300

    submit java-spring-boot-ru/crud

--- java-spring-boot-ru/crud/src/main/java/exercise/mapper/ReferenceMapper.java
+++ java-spring-boot-ru/crud/src/main/java/exercise/mapper/ReferenceMapper.java
@@ -1,18 +1,9 @@
 package exercise.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
-import org.mapstruct.TargetType;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import exercise.model.BaseEntity;
-import jakarta.persistence.EntityManager;
-import org.springframework.stereotype.Component;
-import lombok.RequiredArgsConstructor;
-import exercise.repository.CategoryRepository;
 import exercise.model.Category;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.http.HttpStatus;
+import exercise.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
 // BEGIN
 @Component
@@ -21,9 +12,9 @@ public class ReferenceMapper {
 
     private final CategoryRepository categoryRepository;
 
-    public Category resolveCategory(Long id) {
+    public Category toCategory(Long id) {
         return categoryRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Category not found"));
+                .orElseThrow(() -> new IllegalArgumentException("Category not found"));
     }
 }
 // END
